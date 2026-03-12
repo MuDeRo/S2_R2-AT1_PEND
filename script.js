af42e301770125291c3108ccfca1c07fecc7a76d
MURILO DEMORI RODRIGUES <murilo.rodrigues35@senaisp.edu.br>
2026-03-12 10:36:10 -0300

    Segundo commit

--- script.js
+++ script.js
@@ -1,16 +1,32 @@
-let inputLista = document.querySelector("#itemInput");
+let inputTarefa = document.querySelector("#inputTarefa");
 let btnAdicionar = document.querySelector("#btnAdicionar");
-let lista = document.querySelector("#lista");
+let lista = document.querySelector("#listaTarefas");
+let mensagem = document.querySelector("#mensagem");
 
-btnAdicionar.addEventListener("click", ()=>{
+btnAdicionar.addEventListener("click", () => {
 
-    let itemTexto = inputLista.value; // Pega o texto digitado no campo input
+    let itemTexto = inputTarefa.value; // Pega o texto digitado no campo input
 
     let novoItem = document.createElement("li"); // Cria um novo elemento <li> para a lista
 
     novoItem.innerText = itemTexto; // Coloca o texto digitado dentro do novo item da lista
 
-    lista.appendChild(novoItem); // Adiciona o novo <li> dentro da <ul>
+    
+    if (itemTexto === "") {
+        
+        
+        mensagem.innerText = `Campo vazio! Preencha corretamente`;
+        mensagem.className = `text-danger fw-bold`;
+    } else {
+        
+        let mensageFeedback = document.querySelector("#mensagem");
+        
+        mensageFeedback.innerText = `Item adicionado com sucesso!`
+        mensagem.className = `text-success fw-bold`;
+        lista.appendChild(novoItem); // Adiciona o novo <li> dentro da <ul>
+    }
+
+
 
     inputLista.value = ""; // Limpa o campo input após adicionar o item
 
